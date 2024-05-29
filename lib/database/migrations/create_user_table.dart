776b5bc893c7c0e8fbf8a5cf0a6371aacbde019d
Michael Chiew <michael@airasia.com>
2024-05-29 23:28:59 +0800

    update new users table

--- lib/database/migrations/create_user_table.dart
+++ lib/database/migrations/create_user_table.dart
@@ -5,7 +5,20 @@ class CreateUserTable extends Migration {
   Future<void> up() async {
     super.up();
     await createTable('users', () {
-      id();
+      uuid('id');
+      string('first_name');
+      string('last_name');
+      string('email');
+      string('password');
+      string('remember_token');
+      timeStamp('created_at');
+      timeStamp('updated_at');
     });
+  }
+
+  @override
+  Future<void> down() {
+    super.down();
+    throw UnimplementedError();
   }
 }
